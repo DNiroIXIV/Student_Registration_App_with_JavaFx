d8ccb786f15aed378a026051faeaf46ec08f96e1
DNiroIXIV <dnirodhaixiv@gmail.com>
2025-04-29 22:29:51 +0530

    commit02

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/StudentRegistrationForm.fxml"))));
+        //stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/StudentRegistrationForm.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/LoginForm.fxml"))));
         stage.show();
     }
 }
--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -3,3 +3,4 @@ public class Starter {
         Main.main(args);
     }
 }
+//
